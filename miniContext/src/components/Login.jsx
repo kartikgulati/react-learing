45516671f0734d40c00ae98e7f43bef3374ea740
Kartik Gulati <kartikgulati19@gmail.com>
2024-09-05 16:48:19 -0400

    started again learning

---
+++ miniContext/src/components/Login.jsx
@@ -0,0 +1,35 @@
+import React,{useState, useContext} from 'react'
+import UserContext from '../context/userContext'
+
+const Login = () => {
+   
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState(' ')
+
+    // eslint-disable-next-line no-unused-vars
+    const{setUser} = useContext(UserContext)
+    const handleSubmit =(e)=>{
+        e.preventDefault()
+        setUser({username, password})
+    }
+  return (
+    <div>
+        <h1>Login</h1>
+        <input type="text"
+        value={username} 
+        onChange={(e)=>setUsername(e.target.value)}
+         placeholder='username'
+         />
+
+        <input type="text"
+        value={password}
+         placeholder='password'
+         onChange={(e)=>setPassword(e.target.value)}
+         />
+        <button onClick={handleSubmit}>Submit</button>
+
+    </div>
+  )
+}
+
+export default Login
